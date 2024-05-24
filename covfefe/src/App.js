710c708a4331a8fe6a5bf6ad4dd4cf78fd78074d
Philip Karanja <kpk.my.mailings@gmail.com>
2024-05-24 04:51:07 -0500

    Updates
    Updated MenuItems
    Added Bootstrap

--- covfefe/src/App.js
+++ covfefe/src/App.js
@@ -1,8 +1,32 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import MenuItem from './components/MenuItem';
+import { covfefeMenuItems } from './covfefeData';
 
 function App() {
+  
+  const [menuItems, setMenuItems] = useState(covfefeMenuItems);
+  const [covfefeDessertItems, setCovfefeDessertItems] = useState(covfefeDessertItems);
+  
   return (
     <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="my-3 text-center">Covfefe Shop Menu</h1>
+        </div>
+      </div>
+      <div className="row my-5">
+        <div className="h3">Coffee</div>
+        {menuItems && menuItems.length > 0 && menuItems.map((menuItem) => (
+            <MenuItem key={menuItem.id} item={menuItem.item} price={menuItem.price} image={menuItem.image} altText={menuItem.item} />
+        ))}
+      </div>
+      <div className="row">
+        <div className="h3">Desserts</div>
+        {covfefeDessertItems && covfefeDessertItems.length > 0 && covfefeDessertItems.map((dessertItem) => (
+            <MenuItem key={dessertItem.id} item={dessertItem.item} price={dessertItem.price} image={dessertItem.image} altText={dessertItem.item} />
+        ))}
+      </div>
       
     </div>
   );
--- covfefe/src/components/MenuItem.js
+++ covfefe/src/components/MenuItem.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
-export default function MenuItem() {
+export default function MenuItem({image, altText, price, item}) {
   return (
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+    <div className="col-4"> 
+      <div className="card" style={{ maxWidth: '18rem'}}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={image} className="card-img-top" alt={altText}/>
+              <div className="col-md-8">
+                <div className ="card-body">
+              <h5 className="card-title">{item}</h5>
+              <p className ="card-text">{`$${price}`}</p>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">An item</li>
+              <li className="list-group-item">A second item</li>
+              <li className="list-group-item">A third item</li>
+            </ul>
+            <div className ="card-body">
+            </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
