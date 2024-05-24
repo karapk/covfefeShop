4e0eb71ecf42fab19368431d8d43d8dfa6e61ae8
Philip Karanja <kpk.my.mailings@gmail.com>
2024-05-24 03:50:53 -0500

    covfefeData update

--- covfefe/src/components/MenuItem.js
+++ covfefe/src/components/MenuItem.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function MenuItem() {
   return (
-    <div>MenuItem</div>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
   )
 }
---
+++ covfefe/src/covfefeData.js
@@ -0,0 +1,17 @@
+export const covfefeMenuItems = [
+    { id: 1, item: "Cold brew Coffee", price: 1, image: 'https://github.com/karapk/covfefeShop/blob/main/Cold%20brew%20coffee.jpeg'},
+    { id: 2, item: "Espresso", price: 2, image: 'https://github.com/karapk/covfefeShop/blob/main/Espresso.jpeg'},
+    { id: 3, item: "Frappuccino", price: 3, image: 'https://github.com/karapk/covfefeShop/blob/main/Frappuccino.jpeg'},
+    { id: 4, item: "Latte", price: 2, image: 'https://github.com/karapk/covfefeShop/blob/main/Latte.jpeg'},
+    { id: 5, item: "Turkish coffee", price: 4, image: 'https://github.com/karapk/covfefeShop/blob/main/Turkish%20coffee.jpeg'},
+    { id: 6, item: "Vietnamese iced coffee", price: 2, image: 'https://github.com/karapk/covfefeShop/blob/main/Vietnamese%20Iced%20coffee.jpeg'}
+]
+
+export const covfefeDessertItems = [
+    { id: 1, item: "Mandazi", price: 3, image: 'https://github.com/karapk/covfefeShop/blob/main/Mandazi.jpeg'},
+    { id: 2, item: "Croissant", price: 2, image: 'https://github.com/karapk/covfefeShop/blob/main/Croissant.jpeg'},
+    { id: 3, item: "Mandazi", price: 1, image: 'https://github.com/karapk/covfefeShop/blob/main/Mandazi.jpeg'},
+    { id: 4, item: "Affogato", price: 4, image: 'https://github.com/karapk/covfefeShop/blob/main/Affogato.jpeg'},
+    { id: 5, item: "Samosa", price: 4, image: 'https://github.com/karapk/covfefeShop/blob/main/Samosa.jpeg'},
+    { id: 6, item: "Chapati", price: 5, image: 'https://github.com/karapk/covfefeShop/blob/main/Chapati.jpeg'}
+]
