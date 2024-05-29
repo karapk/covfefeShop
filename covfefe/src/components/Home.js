16cf01e4ab9cf396956857b2ad39210bd4ed7893
Philip Karanja <kpk.my.mailings@gmail.com>
2024-05-29 18:07:43 -0500

    Added weather function

--- covfefe/src/components/Home.js
+++ covfefe/src/components/Home.js
@@ -1,22 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import MenuItem from './MenuItem';
+import Weather from './Weather';
 import { covfefeMenuItems, covfefeDessertItems } from '../covfefeData';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import ShoppingCart from './ShoppingCart';
 
 export default function Home() {
+  const weatherApiKey = '8a4a659f034f1c9c5b0e86ce24c04de9';
   const [menuItems, setMenuItems] = useState([]);
   const [dessertItems, setDessertItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [cartVisible, setCartVisible] = useState(false);
+  const [userCity, setUserCity] = useState(null);
+  const [currentTemp, setCurrentTemp] = useState(null);
 
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log('position', position)
+          const { latitude, longitude } = position.coords;
+          // //Anchorage 61.2181, -149.9003
+          // getWeatherData(61.2181, -149.9003)
+          // // Death Valley 36.4614, -116.8656
+          // getWeatherData(36.4614, -116.8656)
+          // //browser
+          getWeatherData(latitude, longitude)
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+        }
+      );
+    }
+  }, [])
+
+  async function getWeatherData(lat, lon){
+    console.log(lat)
+    console.log(lon)
+    let weatherAPIResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`);
+    console.log('weatherAPIResponse', weatherAPIResponse)
+    if(weatherAPIResponse.status != 200){
+      alert('could not get weather data')
+    }
+    let jsonData = await weatherAPIResponse.json()
+    console.log('jsonData', jsonData)
+    setUserCity(jsonData.name)
+    setCurrentTemp(Math.round(jsonData.main.temp))
+  }
+  
   useEffect(() => {
     setTimeout(() => {
       setMenuItems(covfefeMenuItems);
       setDessertItems(covfefeDessertItems);
     }, 1000);
   }, []);
+
 
   const addToCart = (item) => {
     setCartItems((prevItems) => {
@@ -44,6 +83,7 @@ export default function Home() {
             <h1 className="my-3 text-center">Covfefe Shop Menu</h1>
           </div>
         </div>
+        <Weather currentTemp={currentTemp} userCity={userCity} />
         <div className="row my-5">
           <div className="col-12">
             <h3 className="text-center">Coffee</h3>
