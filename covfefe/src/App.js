9381c9d084ebfe80962ea7b64af241a96d90e575
Philip Karanja <kpk.my.mailings@gmail.com>
2024-06-09 23:49:45 -0500

    Update App.js

--- covfefe/src/App.js
+++ covfefe/src/App.js
@@ -1,31 +1,44 @@
-// import React from 'react';
-import './App.css'
-import Home from './pages/Home'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Layout from './components/Layout'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-
-    children: [
-      {path:"", element: <Home />},
-      {path: "AboutUs",element:<AboutUs/> },
-    ]
-  }
-]);
+import Navbar from './components/Navbar';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
+  const [cartVisible, setCartVisible] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const toggleCart = () => {
+    setCartVisible(!cartVisible);
+  };
+
+  const addToCart = (item) => {
+    setCartItems((prevItems) => {
+      const existingItem = prevItems.find((cartItem) => cartItem.id === item.id);
+      if (existingItem) {
+        return prevItems.map((cartItem) =>
+          cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+        );
+      } else {
+        return [...prevItems, { ...item, quantity: 1 }];
+      }
+    });
+  };
+
   return (
-    <>
-    <RouterProvider  router={router} />
-    </>
+    <Router>
+      <Navbar toggleCart={toggleCart} cartItems={cartItems} /> {/* Pass toggleCart and cartItems to Navbar */}
+      <Routes>
+        <Route path="/" element={<Home addToCart={addToCart} />} /> {/* Pass addToCart to Home */}
+        <Route path="/aboutUS" element={<AboutUs />} />
+      </Routes>
+      {cartVisible && (
+        <ShoppingCart cartItems={cartItems} toggleCart={toggleCart} /> /* Conditionally render ShoppingCart */
+      )}
+    </Router>
   );
 }
 
 export default App;
+  
