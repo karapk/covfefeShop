aff54540c20e904b1df48f637ed772c17939d507
Philip Karanja <kpk.my.mailings@gmail.com>
2024-06-11 04:30:40 -0500

    completed shopping cart and router

--- covfefe/src/pages/Home.js
+++ covfefe/src/pages/Home.js
@@ -1,15 +1,13 @@
+// src/pages/Home.js
 import React, { useState, useEffect } from 'react';
 import MenuItem from '../components/MenuItem';
 import Weather from '../components/Weather';
 import { covfefeMenuItems, covfefeDessertItems } from '../covfefeData';
-import ShoppingCart from '../components/ShoppingCart'; // Import the ShoppingCart component
 
-export default function Home() {
+export default function Home({ addToCart }) {
   const weatherApiKey = '8a4a659f034f1c9c5b0e86ce24c04de9';
   const [menuItems, setMenuItems] = useState([]);
   const [dessertItems, setDessertItems] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartVisible, setCartVisible] = useState(false);
   const [userCity, setUserCity] = useState(null);
   const [currentTemp, setCurrentTemp] = useState(null);
 
@@ -46,50 +44,30 @@ export default function Home() {
     }, 1000);
   }, []);
 
-  const addToCart = (item) => {
-    setCartItems((prevItems) => {
-      const existingItem = prevItems.find((cartItem) => cartItem.item === item.item);
-      if (existingItem) {
-        return prevItems.map((cartItem) =>
-          cartItem.item === item.item ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
-        );
-      } else {
-        return [...prevItems, { ...item, quantity: 1 }];
-      }
-    });
-  };
-
-  const toggleCart = () => {
-    setCartVisible(!cartVisible);
-  };
-
   return (
-    <>
-      <div className="container">
-        <Weather currentTemp={currentTemp} userCity={userCity} />
-        <div className="row">
-          <div className="col-12">
-            <h1 className="my-3 text-center">Covfefe Shop Menu</h1>
-          </div>
-        </div>
-        <div className="row my-5">
-          <div className="col-12">
-            <h3 className="text-center">Coffee</h3>
-          </div>
-          {menuItems.length > 0 && menuItems.map((menuItem) => (
-            <MenuItem key={menuItem.id} item={menuItem.item} price={menuItem.price} image={menuItem.image} altText={menuItem.item} addToCart={() => addToCart(menuItem)} />
-          ))}
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <h3 className="text-center">Desserts</h3>
-          </div>
-          {dessertItems.length > 0 && dessertItems.map((dessertItem) => (
-            <MenuItem key={dessertItem.id} item={dessertItem.item} price={dessertItem.price} image={dessertItem.image} altText={dessertItem.item} addToCart={() => addToCart(dessertItem)} />
-          ))}
+    <div className="container">
+      <Weather currentTemp={currentTemp} userCity={userCity} />
+      <div className="row">
+        <div className="col-12">
+          <h1 className="my-3 text-center">Covfefe Shop Menu</h1>
         </div>
       </div>
-      <ShoppingCart cartItems={cartItems} isVisible={cartVisible} toggleCart={toggleCart} /> Include ShoppingCart
-    </>
+      <div className="row my-5">
+        <div className="col-12">
+          <h3 className="text-center">Coffee</h3>
+        </div>
+        {menuItems.length > 0 && menuItems.map((menuItem) => (
+          <MenuItem key={menuItem.id} item={menuItem.item} price={menuItem.price} image={menuItem.image} altText={menuItem.item} addToCart={() => addToCart(menuItem)} />
+        ))}
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <h3 className="text-center">Desserts</h3>
+        </div>
+        {dessertItems.length > 0 && dessertItems.map((dessertItem) => (
+          <MenuItem key={dessertItem.id} item={dessertItem.item} price={dessertItem.price} image={dessertItem.image} altText={dessertItem.item} addToCart={() => addToCart(dessertItem)} />
+        ))}
+      </div>
+    </div>
   );
 }
